6fb1d56e8e667863188f39bd43382d4903142902
Niz46 <paujohn66@gmail.com>
2025-07-15 12:26:06 +0100

    logo commit

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import LOGO from "/logo.svg";
+import LOGO from "/logo.png";
 import Login from "../../components/Auth/Login";
 import SignUp from "../../components/Auth/SignUp";
 
@@ -10,7 +10,12 @@ const AdminLogin = () => {
     <>
       <div className="bg-white py-5 border-b border-gray-50">
         <div className="container mx-auto">
-          <img src={LOGO} alt="Logo" className="h-[26px] pl-6" />
+          <div className="flex items-center justify-center gap-10">
+            <img src={LOGO} alt="logo" className="h-[24px] md:h-[36px]" />
+            <span className="font-poppins font-extrabold text-[24px] md:text-4xl uppercase tracking-wider inline-block bg-gradient-to-r from-cyan-400 via-sky-500 to-indigo-600 bg-clip-text text-transparent drop-shadow-[0_4px_10px_rgba(0,0,0,0.25)] transform -skew-y-2">
+              UAACAI
+            </span>
+          </div>
         </div>
       </div>
 
@@ -19,10 +24,7 @@ const AdminLogin = () => {
           {currentPage === "login" ? (
             <Login setCurrentPage={setCurrentPage} />
           ) : (
-            <SignUp
-              setCurrentPage={setCurrentPage}
-              showAdminToken={true}
-            />
+            <SignUp setCurrentPage={setCurrentPage} showAdminToken={true} />
           )}
         </div>
       </div>
