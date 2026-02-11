0e7c8e7f9dd38887e128adf30a5b0e94f3c4ec75
Niz46 <paujohn66@gmail.com>
2026-02-11 21:15:33 +0100

    fix: Update gradient class to linear for logo text styling

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -12,7 +12,7 @@ const AdminLogin = () => {
         <div className="container mx-auto">
           <div className="flex items-center ml-5 gap-4">
             <img src={LOGO} alt="logo" className="h-18 md:h-19" />
-            <span className="font-poppins font-extrabold text-[24px] md:text-4xl uppercase tracking-wider inline-block bg-gradient-to-r from-cyan-400 via-sky-500 to-indigo-600 bg-clip-text text-transparent drop-shadow-[0_4px_10px_rgba(0,0,0,0.25)] transform -skew-y-2">
+            <span className="font-poppins font-extrabold text-[24px] md:text-4xl uppercase tracking-wider inline-block bg-linear-to-r from-cyan-400 via-sky-500 to-indigo-600 bg-clip-text text-transparent drop-shadow-[0_4px_10px_rgba(0,0,0,0.25)] transform -skew-y-2">
               UAACAI
             </span>
           </div>
